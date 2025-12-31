10a072d030ddbfaa76b45a8c8044f9752d9c76dd
SrishtiGupta3006 <srishtigupta2630@gmail.com>
2025-12-31 05:48:37 +0530

    Updated

--- ExampleScenario/Logging_AV/output_LE_mono.py
+++ ExampleScenario/Logging_AV/output_LE_mono.py
@@ -10,74 +10,12 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
-from helper.product import DFA
+from helper.dfa_definitions import LE_mono_phi1, LE_mono_phi2
 from Source.least_effort_mono import least_effort_monolithic_enforcer
 
 
-# Defining states and transitions ----------------------
-
-states = ['stop', 'start', 'right', 'left', 'forward', 'back']
-
-transition_dict = {
-    ('start', 'r'): 'right',
-    ('start', 'l'): 'left',
-    ('start', 'f'): 'forward',
-    ('start', 'b'): 'back',
-    ('start', 's'): 'stop',
-    
-    ('right', 'r'): 'right',
-    ('right', 'l'): 'left',
-    ('right', 'f'): 'forward',
-    ('right', 'b'): 'back',
-    ('right', 's'): 'stop',
-    
-    ('left', 'r'): 'right',
-    ('left', 'l'): 'left',
-    ('left', 'f'): 'forward',
-    ('left', 'b'): 'back',
-    ('left', 's'): 'stop',
-    
-    ('forward', 'r'): 'right',
-    ('forward', 'l'): 'left',
-    ('forward', 'f'): 'forward',
-    ('forward', 'b'): 'back',
-    ('forward', 's'): 'stop',
-    
-    ('back', 'r'): 'right',
-    ('back', 'l'): 'left',
-    ('back', 'f'): 'forward',
-    ('back', 'b'): 'back',
-    ('back', 's'): 'stop',
-    
-    ('stop', 'r'): 'stop',
-    ('stop', 'l'): 'stop',
-    ('stop', 'f'): 'stop',
-    ('stop', 'b'): 'stop',
-    ('stop', 's'): 'stop',
-    
-}
-
-# Define DFAs ----------------------
-
-phi1 = DFA(
-    "Property1",
-    ['r', 'l', 'f', 'b', 's'],
-    states,
-    'start',
-    lambda q: q in ['right'],               # accepting state
-    lambda q, a: transition_dict[(q, a)],
-    ['right']
-)
-
-phi2 = DFA(
-    "Property2",
-    ['r', 'l', 'f', 'b', 's'],
-    states,
-    'start',
-    lambda q: q in ['left'],                # accepting state
-    lambda q, a: transition_dict[(q, a)],
-    ['left']
-)
+phi1 = LE_mono_phi1()
+phi2 = LE_mono_phi2()
 
 # Least Effort Monolithic Enforcer (OR-Product) ------------------------
 enforcer_fn = least_effort_monolithic_enforcer("OR_Product", phi1, phi2)
--- ExampleScenario/Logging_AV/output_strict_parallel.py
+++ ExampleScenario/Logging_AV/output_strict_parallel.py
@@ -9,46 +9,15 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
 from Source.strict_parallel import StrictParallelEnforcer
-from helper.product import DFA
-
-# -------------------------------------------------
-# Define DFAs (phi1, phi2)
-# -------------------------------------------------
-
-states = ['stop', 'start', 'right', 'left', 'forward', 'back']
-
-transition_dict = {
-    ('start','r'):'right', ('start','l'):'left', ('start','f'):'forward', ('start','b'):'back', ('start','s'):'stop',
-    ('right','r'):'right', ('right','l'):'left', ('right','f'):'forward', ('right','b'):'back', ('right','s'):'stop',
-    ('left','r'):'right', ('left','l'):'left', ('left','f'):'forward', ('left','b'):'back', ('left','s'):'stop',
-    ('forward','r'):'right', ('forward','l'):'left', ('forward','f'):'forward', ('forward','b'):'back', ('forward','s'):'stop',
-    ('back','r'):'right', ('back','l'):'left', ('back','f'):'forward', ('back','b'):'back', ('back','s'):'stop',
-    ('stop','r'):'stop', ('stop','l'):'stop', ('stop','f'):'stop', ('stop','b'):'stop', ('stop','s'):'stop',
-}
-
-phi1 = DFA(
-    "phi1",
-    ['r','l','f','b','s'],
-    states,
-    'start',
-    lambda q: q == 'right',
-    lambda q,a: transition_dict[(q,a)],
-    ['right']
+from helper.dfa_definitions import (
+    Strict_parallel_phi1,
+    Strict_parallel_phi2
 )
 
-phi2 = DFA(
-    "phi2",
-    ['r','l','f','b','s'],
-    states,
-    'start',
-    lambda q: q == 'left',
-    lambda q,a: transition_dict[(q,a)],
-    ['left']
-)
+# Load property DFAs
 
-# -------------------------------------------------
-# Strict Parallel Enforcer
-# -------------------------------------------------
+phi1 = Strict_parallel_phi1()
+phi2 = Strict_parallel_phi2()
 
 enforcer = StrictParallelEnforcer([phi1, phi2])
 
